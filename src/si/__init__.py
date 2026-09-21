f1a8ff4c76f955253f78fa237a871287779c1e7a
Sara Sousa <saranicole.sousa@gmail.com>
2026-09-21 11:41:08 +0100

    Adding my co-authorship to the package

--- src/si/__init__.py
+++ src/si/__init__.py
@@ -1,9 +1,9 @@
-__author__ = "Diogo Cachetas"
-__credits__ = ["Diogo Cachetas", "João Capela", "João Correia", "Fernando Cruz", "Vítor Pereira"]
+__author__ = "Sara Sousa" 
+__credits__ = ["Sara Sousa"]
 __license__ = "Apache License 2.0"
 __version__ = "0.0.1"
-__maintainer__ = "Diogo Cachetas"
-__email__ = "diogo.cachetas@inl.int"
+__maintainer__ = "Sara_Sousa"
+__email__ = "pg59766@uminho.pt"
 
 
 
